0b7cc5510dd3353ec5542e1d65452fc40b1a6fa8
Blake Kruppa <blake.kruppa@wallawalla.edu>
2018-04-12 00:38:00 -0400

    merging classes code (#448)
    * moved batch, trainer, trainee urls to Hydra-Client
    
    * trianer service created
    
    * added karma
    
    * added trainee service that is similar to the Caliber trainee service
    
    * populate and change current
    
    * trainer service api
    
    * added the hydra client
    
    * removed unused code
    
    * readded trainee and url service to new hydra-client
    
    * created hydra batch service
    
    * configured the hydra client to take in the base context
    
    * fixed the caliber categories component to use Hydra
    
    * couldn't get the app to build when trying to create hydra client the .forRoot way
    
    * rearranged hydra client services
    
    * added httpclient to batch spec
    
    * refactored trainee and url service
    
    * slight service changes
    
    * readded service to providers
    
    * created batch, user, trainer, and trainee entities
    
    * used hydra client trainer service within caliber
    
    * added basic CRUD funcitonallity on server side
    
    * added batch urls
    
    * trainer profile working
    
    * trainer profiles now functional
    
    * Fixed batch entity and created new ones for trainer, trainee, and user
    
    * API generated, GET /batches gets a 403
    
    * finished API and began connectng client side with API
    
    * Edited hydra-trainee-service comments
    
    * added working example on caliber manage component, added user entity
    
    * Updated all Caliber components to use our hydra batch service and entity
    
    * small changes to trainers component
    
    * slight lint changes in TrackForce and switched out our service wherever I could
    
    * changed entity to use first and last name, and trainer components to match
    
    * working with displaying tiers
    
    * preare for code review
    
    * fixed showing roles, formerly tiers
    
    * some trainer profile things working
    
    * removed microservice
    
    * fixed changes in code review, removed unused code and spacing, added password field to create trainer modal
    
    * trainer profile edit modal uses first and last name
    
    * changed endpoints, fixed package.json, and implemented fetching data example in manage component
    
    * adding new trainer dynamically updates table
    
    * merging batch with the rest and uncommenting certain methods
    
    * added karma/jasmine test suite for endpoints
    
    * all client sides merged together
    
    * changed every instance of batch to hydrabatch, trainee to hydratrainee
    
    * added first karma test!
    
    * implemented RabbitMQ and made code review corrections
    
    * begin testing batchservice with jasmine
    
    * continued with jasmine testing
    
    * changed roles to strings and update on profile works
    
    * all of my karma services work right charles?
    
    * completed jasmine spec for batch service, for now
    
    * Changed envrionment context to point to Zuul
    
    * updated environment url, fixed hydra trainee tests
    
    * added comprehensive trainee service test suite
    
    * slightly modified trainee service tests
    
    * Fixed batch management table
    
    * added better test suite for trainer and batch
    
    * added to the hydra client readme
    
    * getting trainees on trainer's batches
    
    * removed extra closing tags

--- client-side/src/app/hydra-client/services/urls/url.service.ts
+++ client-side/src/app/hydra-client/services/urls/url.service.ts
@@ -21,8 +21,42 @@ export class UrlService {
     update: () => `${this.context}skills`,
   };
 
+ /**
+   * Endpoints for batches
+   */
+
+ batches = {
+    fetchAllByTrainer: () => `${this.context}batches/trainers`,
+    fetchAllByTrainerId: (id: number) => `${this.context}batches/trainers/${id}`,
+    fetchAll: () => `${this.context}batches`,
+    save: () => `${this.context}batches`,
+    update: () => `${this.context}batches`,
+    delete: (batchId) => `${this.context}batches/${batchId}`
+};
+
+  /**
+   * Endpoints for trainees
+   */
+  trainees = {
+    findAllByBatchAndStatus: (id: number, status: string) => `${this.context}trainees/batch/${id}/status/${status}`,
+    save: () => `${this.context}trainees`,
+    update: () => `${this.context}trainees`,
+    delete: (traineeId: number) => `${this.context}trainees/${traineeId}`,
+  };
+
+  /**
+   * Endpoints for trainers
+   */
+  trainers = {
+    fetchByEmail: (email: string) => `${this.context}trainers/email/${email}/`,
+    fetchAll: () => `${this.context}trainers`,
+    save: () => `${this.context}trainers`,
+    update: () => `${this.context}trainers`,
+    getTitles: () => `${this.context}trainers/titles`,
+    getRoles: () => `${this.context}trainers/roles`,
+
+  };
   constructor() {
     this.context = environment.hydraContext;
   }
-
 }
