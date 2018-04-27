fb54dd2e20c7f67cc54e0b73de2a31fd5ace1512
Jordan Young <jryoung1330@gmail.com>
2018-04-27 16:00:25 -0400

    qcstatus-service completed

--- client-side/src/app/portals/Caliber/services/qcstatus.service.spec.ts
+++ client-side/src/app/portals/Caliber/services/qcstatus.service.spec.ts
@@ -1,14 +1,33 @@
 import { TestBed, inject } from '@angular/core/testing';
 import { QCStatusService } from './qcstatus.service';
 import { Dependencies } from '../caliber.test.module';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { Observable } from 'rxjs/Observable';
 
-
-xdescribe('QCStatusService', () => {
+fdescribe('QCStatusService', () => {
   beforeEach(() => {
-    TestBed.configureTestingModule(Dependencies);
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [QCStatusService]
+    });
   });
 
   it('should be created', inject([QCStatusService], (service: QCStatusService) => {
     expect(service).toBeTruthy();
   }));
+
+  // test for getList()
+  it('getList() should return an observable', inject([QCStatusService], (service: QCStatusService) => {
+    expect(service.getList()).toBeTruthy();
+    const listS: Observable<string[]> = service.getList();
+  }));
+
+  // test for fetchAll()
+  it('fetchAll() should return an observable', inject([QCStatusService], (service: QCStatusService) => {
+    service.fetchAll().subscribe((obs) => {
+      expect(obs).toBeTruthy();
+    });
+  }));
+
 });
+
